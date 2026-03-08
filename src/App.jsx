3825d82be1939b2a4f9c08c074ae8f6d9c754185
Mukuakuiza <hinomukuakuiza12@gmail.com>
2026-03-08 23:45:24 +0200

    installed react-router-dom, react-hok-form, created pages home,cart, checkout. all the basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-
+import "./App.css";
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home.jsx";
+import Auth from "./pages/Auth.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import NavBar from "./components/NavBar.jsx";
 function App() {
 
   return (
-    <>
-        <h1>Hello React</h1>
-    </>
+    <div className="app">
+        <NavBar/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/auth" element={<Auth/>}/>
+                <Route path="/checkout" element={<Checkout/>}/>
+        </Routes>
+    </div>
   )
 }
 
